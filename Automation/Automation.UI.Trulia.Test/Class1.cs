625f3425e324f650e9cc3e98206ce0a1f0a7b0b4
Oleksiy Kyrylenko <oleksiykyrylenkoautomation@gmail.com>
2019-07-28 02:17:33 -0400

    added runsettings and config files

---
+++ Automation/Automation.UI.Trulia.Test/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Automation.UI.Trulia.Test
+{
+    public class Class1
+    {
+
+        [Test]
+        public void Test()
+        {
+            var env = TestContext.Parameters["Environment"];
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true,true).Build();
+
+            var v = config.GetSection(env)["BaseUrl"];
+            var v1 = config[$"{env}:BaseUrl"];
+            var url = config.GetSection(env).GetChildren().Where(val => val.Key.Equals("BaseUrl")).First().Value;
+
+            var driver = new ChromeDriver(@"C:\Users\Alex\Desktop");
+            driver.Navigate().GoToUrl($"https://www.{url}.com");
+            Thread.Sleep(1000);
+            driver.Quit();
+        }
+    }
+}
