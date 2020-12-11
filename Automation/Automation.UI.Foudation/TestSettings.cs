66ce9338c3df04e7c428096d51a0e1d198b43ade
oleksiykyrylenko <oleksiykyrylenkoautomation@gmail.com>
2020-12-11 14:20:53 -0500

    update

--- Automation/Automation.UI.Foudation/TestSettings.cs
+++ Automation/Automation.UI.Foudation/TestSettings.cs
@@ -3,45 +3,28 @@ using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using System;
 
-namespace Automation.UI.Foudation
-{
-    public static class TestSettings
-    {
+namespace Automation.UI.Foudation    {
+    public static class TestSettings        {
         private static string baseUrl = null;
         private static Enums.Environment env;
-        private static Browser browser;
+        private static BrowserType browser;
         private static Location location;
         private static Platform platform;
         private static IConfiguration config = null;
 
-        public static IConfiguration Configuration=>config = config ??
-            new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        public static IConfiguration Configuration => config = config ??
+            new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();        static string v = TestContext.Parameters["Browser"];        public static BrowserType Browser => browser = browser != BrowserType.None ? browser :
+                    (BrowserType)Enum.Parse(typeof(BrowserType), TestContext.Parameters["Browser"]);
 
-        //public static Browser Browser
-        //{
-        //    get
-        //    {
-        //        browser = browser != Browser.None ? browser :
-        //            (Browser)Enum.Parse(typeof(Browser), TestContext.Parameters["Browser"]);
-        //        return browser;
-        //    }
-        //}
-        public static Browser Browser=> browser = browser != Browser.None ? browser :
-                    (Browser)Enum.Parse(typeof(Browser), TestContext.Parameters["Browser"]);
-
-        public static Enums.Environment Environment=> env = env != Enums.Environment.None ? env :
+        public static Enums.Environment Environment => env = env != Enums.Environment.None ? env :
                     (Enums.Environment)Enum.Parse(typeof(Enums.Environment), TestContext.Parameters["Environment"]);
 
         public static Location Location => location = location != Location.None ? location :
                     (Location)Enum.Parse(typeof(Location), TestContext.Parameters["Location"]);
 
-        public static Platform Plaform =>  platform =  platform != Platform.None ? platform :
+        public static Platform Plaform => platform = platform != Platform.None ? platform :
                     (Platform)Enum.Parse(typeof(Platform), TestContext.Parameters["Platform"]);
 
-        public static string BaseUrl=> baseUrl ?? (Configuration.GetSection(Environment.ToString()) ??
+        public static string BaseUrl => baseUrl ?? (Configuration.GetSection(Environment.ToString()) ??
                     throw new Exception("The environment is not set in runsettings file." +
-                    "Please update your runsettings file"))["BaseUrl"];
-                //var url = config[$"{Environment.ToString()}:BaseUrl"];
-                //var url = config.GetSection(Environment.ToString()).GetChildren().Where(val => val.Key.Equals("BaseUrl")).First().Value;
-    }
-}
+                    "Please update your runsettings file"))["BaseUrl"];        }    }
