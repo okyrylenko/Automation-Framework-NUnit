e4827688a4dc6dd8050a6e9edf039ab2f3590da1
oleksiykyrylenko <oleksiykyrylenkoautomation@gmail.com>
2020-12-14 10:20:02 -0500

    updates

---
+++ Automation/Automation.UI.Foudation/Extenstions/ByExtenstions.cs
@@ -0,0 +1,1 @@
+﻿using System;using OpenQA.Selenium;namespace Automation.UI.Foudation.Extenstions    {    public static class ByExtenstions        {        public static string GetSelector(this By by) => by.ToString().Split(": ")[1];        }    }
