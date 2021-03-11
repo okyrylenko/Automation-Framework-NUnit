48ce33786bfb1d6ffa4332087d9ce23c396c1d7a
okyrylenko <40716729+okyrylenko@users.noreply.github.com>
2021-03-10 19:41:24 -0500

    Update HomePage.cs
    updated the Mac line break to make code readable in plain text editor

--- Automation/Automation.UI.Trulia.Pages/HomePage.cs
+++ Automation/Automation.UI.Trulia.Pages/HomePage.cs
@@ -1,1 +1,25 @@
-﻿using System;using Automation.UI.Foudation;using Automation.UI.IHS.Pages.Components;using OpenQA.Selenium;using OpenQA.Selenium.Support.UI;namespace Automation.UI.IHS.Pages    {    public class HomePage : BasePage        {        IWebDriver driver;        WebDriverWait wait;        public HomePage(IWebDriver driver, WebDriverWait wait) : base(driver, wait)            {            this.driver = driver;            this.wait = wait;            }        public Options OptionsPane()            {            return new Options(this.driver, this.wait);            }        }    }
+using System;
+using Automation.UI.Foudation;
+using Automation.UI.IHS.Pages.Components;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Automation.UI.IHS.Pages
+    {
+    public class HomePage : BasePage
+        {
+        IWebDriver driver;
+        WebDriverWait wait;
+        public HomePage(IWebDriver driver, WebDriverWait wait) : base(driver, wait)
+            {
+            this.driver = driver;
+            this.wait = wait;
+
+            }
+
+        public Options OptionsPane()
+            {
+            return new Options(this.driver, this.wait);
+            }
+        }
+    }
